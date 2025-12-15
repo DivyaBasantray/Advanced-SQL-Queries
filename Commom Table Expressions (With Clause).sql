193af4a71b5a6522eec0539b030e09ff5c128bda
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2025-12-15 22:09:53 +0530

    Add files via upload

---
+++ Commom Table Expressions (With Clause).sql
@@ -0,0 +1,66 @@
+-- CTEs
+
+-- A CTE allows to define a subquery block that can be referenced within the main query. 
+-- It is particularly useful for recursive queries or queries that require referencing a higher level.
+
+WITH CTE_Example AS 
+(
+SELECT gender, AVG(salary), MAX(salary), MIN(salary), COUNT(salary)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT *
+FROM CTE_Example;
+
+WITH CTE_Example AS 
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT AVG(avg_sal)
+FROM CTE_Example;
+
+-- ALTERNATIVE WAY TO GET THE SAME RESULTS USING SUBQUERY
+
+SELECT AVG(avg_sal)
+FROM 
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+) example_subquery;
+
+WITH CTE_Example AS 
+(
+SELECT employee_id, gender, birth_date
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+),
+CTE_Example2 AS 
+(
+SELECT employee_id,salary
+FROM employee_salary
+WHERE salary > 50000
+)
+SELECT *
+FROM CTE_Example
+JOIN CTE_Example2
+	ON CTE_Example.employee_id = CTE_Example2.employee_id;
+
+WITH CTE_Example (Gender, Avg_Sal, Max_Sal, Min_Sal, Count_Sal) AS 
+(
+SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT *
+FROM CTE_Example;
